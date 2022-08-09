4d6c30c931eecc1068ea6070330f63428600c76b
Fomafix <fomafix@googlemail.com>
2022-08-09 13:13:36 +0000

    Remove style module 'ext.theme.preferences'
    There is no margin-top on a label.oo-ui-radioOptionWidget:first-child
    in Special:Preferences which must be overwritten by 0.
    
    Change-Id: Ie7eb4c0b5a87cbe16caa5884eae53ffd6a4f7265

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -100,8 +100,6 @@ class Hooks implements
 		) {
 			// Only load this JS on Special:Preferences/Special:GlobalPreferences
 			$out->addModules( 'ext.theme.livepreview' );
-			// Stupid CSS hack
-			$out->addModuleStyles( 'ext.theme.preferences' );
 		}
 	}
 
